aef72fd943d88a03f9808bbc0e90ae3348d0baa0
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-02 18:17:38 +0200

    Make Posts render semi-correctly

---
+++ src/components/Post.tsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import {Button, Card, CardColumns, CardImg, Carousel, CarouselItem} from "react-bootstrap";
+
+interface Props {
+    data: Data
+}
+
+interface Data {
+    hasContent: boolean;
+    hasImages: boolean;
+    title: String;
+    content: String;
+    files: Array<URL>
+}
+
+export class Post extends Component<Props> {
+
+    render() {
+        const {data} = this.props
+        console.log(data)
+        return (
+            <Card style={{width: '18rem'}}>
+                {data.hasImages && <RenderImages images={data.files}/>}
+                <Card.Body>
+                    <Card.Title>{data.title}</Card.Title>
+                    <Card.Text>
+                        Has images: {data.hasImages.toString()}<br/>
+                        Has content: {data.hasContent.toString()}<br/>
+                        {data.content}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+function RenderImages(props: { images: Array<URL> }) {
+    const {images} = props
+
+    if (images.length === 1)
+        return <Card.Img variant="top" src={images[0].toString()}/>
+
+    const items = images.map(el =>
+        <Carousel.Item>
+            <img
+                className={"d-block w-100"}
+                src={el.toString()}
+            />
+        </Carousel.Item>
+    )
+
+    return <Carousel>{items}</Carousel>
+
+}
+
---
+++ src/components/PostsPage.tsx
@@ -0,0 +1,64 @@
+import React, {Component} from "react";
+import api from "../api";
+import {Post} from "./Post";
+import {Alert, CardColumns} from "react-bootstrap";
+
+
+enum Status {
+    loading,
+    success,
+    fail
+}
+
+interface State {
+    data: any;
+    status: Status
+}
+
+export class PostsPage extends Component<any, State> {
+    state: State = {
+        data: undefined,
+        status: Status.loading
+    }
+    componentDidMount = async () => {
+        await api.getAllPosts().then(result => {
+            this.setState({
+                data: JSON.parse(result.data.data),
+                status: Status.success
+            })
+        }).catch(errorMessage => {
+            this.setState({
+                data: JSON.stringify(errorMessage),
+                status: Status.fail
+            })
+        })
+
+    }
+
+    render() {
+        const {status, data} = this.state
+        return (
+            <>
+                <p>status: {status}</p>
+                <h1>List of all posts in the database</h1>
+                {renderByStatus(status, data)}
+            </>
+        );
+    }
+}
+
+function renderByStatus(status: Status, data: any) {
+    switch (status) {
+        case Status.success:
+            const posts = data?.map((data: any, i: any) => <Post data={data} key={i}/>)
+            return <CardColumns>{posts}</CardColumns>
+        case Status.fail:
+            return <Alert>Data could not be loaded</Alert>
+        default:
+            return <Alert>Page is loading</Alert>
+    }
+}
+
+export default PostsPage;
+
+
