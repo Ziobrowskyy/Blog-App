d66be9a683f4fdf5b8ad2685db16452f7f5a5e9e
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-02 18:20:40 +0200

    Add container from react-bootstrap to slightly center content of the page.

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,23 @@ import PostsPage from "./components/PostsPage"
 import AdminPanel from "./components/AdminPanel";
 import About from "./components/About";
 import PageNotFound from "./components/PageNotFound";
+import {Container} from "react-bootstrap";
 
 function App() {
     return (
         <div className="App">
             <Router history={createBrowserHistory()}>
+                <Container>
                 <Header/>
                 <Switch>
-                    <Route exact path={"/"}>
-                        <p>Home page</p>
-                    </Route>
+                    <Route exact path={"/"}><p>Home page</p></Route>
                     <Route path={"/about"} component={About}/>
-                    <Route path={"/posts"} component={PostsList}/>
+                    <Route path={"/posts"} component={PostsPage}/>
                     <Route path={"/admin-panel"} component={AdminPanel}/>
                     <Route path={"*"} component={PageNotFound}/>
                 </Switch>
                 <Footer/>
+                </Container>
             </Router>
         </div>
     );
