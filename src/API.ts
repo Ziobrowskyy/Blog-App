415f5f0beb3b6fdfe41aa2b45def0d2c15549985
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-03 02:51:34 +0200

    Axios API
    * fix type of inserPost data type and change it from any to FormData
    * add content-type: "multipart/form-data" to form API requests to handle to parse and handle them correctly

---
+++ src/API.ts
@@ -0,0 +1,30 @@
+import axios, {AxiosResponse} from "axios";
+
+
+const API = axios.create({
+    baseURL: "http://192.168.0.52:8000",
+
+});
+
+interface IApi {
+    checkConnection: () => Promise<AxiosResponse<any>>;
+    deletePostById: (id: any) => Promise<AxiosResponse<any>>;
+    getPostById: (id: any) => Promise<AxiosResponse<any>>;
+    getAllPosts: () => Promise<AxiosResponse<any>>;
+    insertPost: (data: FormData) => Promise<AxiosResponse<any>>;
+    updatePostById: (id: any, data: any) => Promise<AxiosResponse<any>>
+}
+
+const Apis: IApi = {
+    insertPost: (data) => API.post("/post", data, {
+        headers: {'content-type': 'multipart/form-data'}
+    }),
+    getPostById: (id) => API.get("/posts" + id),
+    getAllPosts: () => API.get("/posts"),
+    updatePostById: (id, data) => API.put("/post/" + id, data, {
+        headers: {'content-type': 'multipart/form-data'}
+    }),
+    deletePostById: (id) => API.delete("/post/" + id),
+    checkConnection: () => API.get("/")
+};
+export default Apis;
