fd459f9ee5f69536099e07a489fe855e327be4d7
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-06 15:56:13 +0200

    User MongoDB Atlas service as Database provider

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,10 +1,16 @@
 import {Collection, MongoClient, MongoError} from "mongodb";
+import dotenv from "dotenv"
 
-const mongoUrl = 'mongodb://localhost:27017'
-const dbName = 'portal'
+dotenv.config()
+
+// const mongoUrl = 'mongodb://localhost:27017'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bolnn.mongodb.net`
+
+const connectionOptions = "?retryWrites=true&w=majority"
+const dbName = 'blog-app'
 const collectionName = "entries"
 
-const client = new MongoClient(`${mongoUrl}/${dbName}/${collectionName}`, {
+const client = new MongoClient(`${uri}/${dbName}${connectionOptions}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -15,16 +21,18 @@ function init(callback: ((err: MongoError) => void) | undefined = undefined) {
     client.connect((err, db) => {
         if (err) {
             console.warn(err);
-            if(callback) callback(err)
+            if (callback) callback(err)
         }
         collection = db.db(dbName).collection(collectionName)
     })
 }
+
 function getDb(): Collection<any> {
-    if(!collection)
+    if (!collection)
         throw new Error("MongoDB collection must be initialized first!")
     return collection
 }
+
 export {
     init,
     getDb
