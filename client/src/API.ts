5461bc06a727aa2774d15c758b222f17b4ac91c7
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-07 17:01:54 +0200

    Rearrange order of functions.

--- client/src/API.ts
+++ client/src/API.ts
@@ -6,23 +6,25 @@ const api = axios.create({
 });
 
 interface IApi {
-    checkConnection: () => Promise<AxiosResponse<NetResponse>>;
-    deletePostById: (id: any) => Promise<AxiosResponse<NetResponse>>;
-    getPostById: (id: any) => Promise<AxiosResponse<NetResponse>>;
     getAllPosts: () => Promise<AxiosResponse<NetResponse>>;
+    getPostById: (id: any) => Promise<AxiosResponse<NetResponse>>;
     insertPost: (data: FormData) => Promise<AxiosResponse<NetResponse>>;
     updatePostById: (id: any, data: any) => Promise<AxiosResponse<NetResponse>>
+    deletePostById: (id: any) => Promise<AxiosResponse<NetResponse>>;
+    login: (data: any) => Promise<AxiosResponse<NetResponse>>
 }
 
 export const Api: IApi = {
-    insertPost: data => api.post("/post", data, {
-        headers: {'content-type': 'multipart/form-data'}
-    }),
-    getPostById: id => api.get("/posts" + id),
     getAllPosts: () => api.get("/posts"),
-    updatePostById: (id, data) => api.put("/post/" + id, data, {
-        headers: {'content-type': 'multipart/form-data'}
+    getPostById: (id) => api.get("/posts" + id),
+    insertPost: (data) => api.post("/post", data, {
+        headers: {"content-type": "multipart/form-data"}
     }),
-    deletePostById: id => api.delete("/post/" + id),
-    checkConnection: () => api.get("/")
+    updatePostById: (id, data) => api.put("/post/" + id, data, {
+        headers: {"content-type": "multipart/form-data"}
+    }),
+    deletePostById: (id) => api.delete("/post/" + id),
+    login: (data) => api.post("/login", data, {
+        headers: {"content-type": "application/json"}
+    })
 };
