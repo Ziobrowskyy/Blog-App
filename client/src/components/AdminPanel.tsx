76c3c0941b8ae000cc81375731523bbaed8b08ca
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-07 13:18:06 +0200

    AdminPanel: Tidy up.

--- client/src/components/AdminPanel.tsx
+++ client/src/components/AdminPanel.tsx
@@ -1,11 +1,13 @@
 import React, {ChangeEvent, Component, FormEvent} from "react";
 import {Button, Form} from "react-bootstrap";
-import Apis from "../API";
+import {Api} from "../API";
+
 interface IState {
     title: string;
     content: string;
     files: any
 }
+
 class AdminPanel extends Component<any, IState> {
     constructor(props: any) {
         super(props);
@@ -27,7 +29,7 @@ class AdminPanel extends Component<any, IState> {
         formData2.append("files", files)
         console.log(formData.get("files"))
         console.log(formData2.get("files"))
-        const response = await Apis.insertPost(formData)
+        const response = await Api.insertPost(formData)
         console.log("Server response:")
         console.log(response)
     }
@@ -38,23 +40,26 @@ class AdminPanel extends Component<any, IState> {
                 <Form onSubmit={this.handleSubmit.bind(this)}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control name="title" type="text" placeholder="Enter title" onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                            this.setState({title: e.target.value})
-                        }}/>
+                        <Form.Control name="title" type="text" placeholder="Enter title"
+                                      onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                          this.setState({title: e.target.value})
+                                      }}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Post content</Form.Label>
-                        <Form.Control name="content" as={"textarea"} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                        <Form.Control name="content" as={"textarea"}
+                                      onChange={(e: ChangeEvent<HTMLInputElement>) => {
                             this.setState({content: e.target.value})
                         }}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formFileMultiple">
                         <Form.Label>Upload photos</Form.Label>
-                        <Form.File name="files" multiple accept="image/*" onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                            this.setState({files: e.target.files})
-                        }}/>
+                        <Form.File name="files" multiple accept="image/*"
+                                   onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                       this.setState({files: e.target.files})
+                                   }}/>
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
