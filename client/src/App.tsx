4b7e58aaeb1ab2ccb489b1971adcee0aa5276330
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-07 13:14:44 +0200

    Add wrapper to change background color of page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import React, {useState} from "react";
 import {createBrowserHistory} from "history"
 import {Route, Router, Switch} from "react-router";
@@ -11,23 +9,23 @@ import PageNotFound from "./components/PageNotFound";
 import {Container} from "react-bootstrap";
 import "./styles/App.scss"
 
-function App() {
+export default function App() {
     const [isLoggedIn, setLoggedIn] = useState(false)
     return (
         <div className="App">
             <Router history={createBrowserHistory()}>
-                <Container>
-                    <Header isLoggedIn={isLoggedIn}/>
-                    <Switch>
-                        <Route exact path={"/"} component={PostsList}/>
-                        <Route path={"/about"} component={About}/>
-                        <Route path={"/admin-panel"} component={AdminPanel}/>
-                        <Route path={"*"} component={PageNotFound}/>
-                    </Switch>
-                </Container>
+                <div className={"main-wrapper"}>
+                    <Container className={"container-md"}>
+                        <Header isLoggedIn={isLoggedIn}/>
+                        <Switch>
+                            <Route exact path={"/"} component={PostsList}/>
+                            <Route path={"/about"} component={About}/>
+                            <Route path={"/admin-panel"} component={AdminPanel}/>
+                            <Route path={"*"} component={PageNotFound}/>
+                        </Switch>
+                    </Container>
+                </div>
             </Router>
         </div>
     );
 }
-
-export default App;
