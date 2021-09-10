12dc41079de8e35b48b840fa6763ea105242a246
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-09-10 14:36:14 +0200

    Client::Form: Add different types of InputFields
    Added file, and textarea input.

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -2,8 +2,9 @@ import React, {ChangeEventHandler, HTMLInputTypeAttribute} from "react";
 
 interface IProps {
     text: string
+    as?: "input" | "textarea" | "file"
     type: HTMLInputTypeAttribute,
-    onChange: ChangeEventHandler<HTMLInputElement>
+    onChange: ChangeEventHandler<HTMLInputElement> | ChangeEventHandler<HTMLTextAreaElement>
 }
 
 export default class FormField extends React.Component<IProps> {
@@ -11,14 +12,35 @@ export default class FormField extends React.Component<IProps> {
         type: "text"
     }
 
+    RenderByType() {
+        const onChange = this.props.onChange
+        switch(this.props.as) {
+            case("textarea"):
+                return <TextAreaInput onChange={onChange as ChangeEventHandler<HTMLTextAreaElement>}/>
+            case("file"):
+                return <FileInput onChange={onChange as ChangeEventHandler<HTMLInputElement>}/>
+            case("input"):
+            default:
+                return <DefaultInput type={this.props.type} onChange={onChange as ChangeEventHandler<HTMLInputElement>}/>
+        }
+    }
     render() {
         return (
             <div className={"form-field"}>
                 <label>
                     <span>{this.props.text}</span>
-                    <input type={this.props.type} onChange={this.props.onChange}/>
+                    {this.RenderByType()}
                 </label>
             </div>
         )
     }
 }
+
+const DefaultInput = (props: {type: HTMLInputTypeAttribute, onChange: ChangeEventHandler<HTMLInputElement>}) =>
+    <input type={props.type} onChange={props.onChange}/>
+
+const TextAreaInput = (props: {onChange: ChangeEventHandler<HTMLTextAreaElement>}) =>
+    <textarea onChange={props.onChange}/>
+
+const FileInput = (props: {onChange: ChangeEventHandler<HTMLInputElement>}) =>
+    <input type={"file"} multiple accept={"image/*"} onChange={props.onChange}/>
