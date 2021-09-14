b2ec14bf61184fe2d3d0a08406ea81d86adac98f
kisia1234 <76399569+kisia1234@users.noreply.github.com>
2021-09-14 18:27:32 +0200

    Client::AdminPanel: Use new FormField and clear content of form after submitting.

--- client/src/components/AdminPanel.tsx
+++ client/src/components/AdminPanel.tsx
@@ -1,18 +1,22 @@
-import React, {ChangeEvent, Component, FormEvent} from "react"
+import React, {ChangeEvent, Component, createRef, FormEvent, RefObject} from "react"
 import {Api} from "../API"
 import Form from "./Form"
-import FormField from "./FormField"
+import {FormFileInput, FormTextArea, FormTextInput} from "./FormField"
 import FormButton from "./FormButton"
 import FormHeader from "./FormHeader"
 import "../styles/AdminPanel.scss"
 
 interface IState {
-    title: string;
-    content: string;
+    title: string
+    content: string
     files: any
 }
 
 class AdminPanel extends Component<any, IState> {
+    private readonly titleInputRef: RefObject<HTMLInputElement>
+    private readonly contentInputRef: RefObject<HTMLTextAreaElement>
+    private readonly fileInputRef: RefObject<HTMLInputElement>
+
     constructor(props: any) {
         super(props)
 
@@ -21,14 +25,32 @@ class AdminPanel extends Component<any, IState> {
             content: "",
             files: [],
         }
+
+        this.titleInputRef = createRef<HTMLInputElement>()
+        this.contentInputRef = createRef<HTMLTextAreaElement>()
+        this.fileInputRef = createRef<HTMLInputElement>()
+        this.handleSubmit.bind(this)
     }
 
     async handleSubmit(event: FormEvent) {
         event.preventDefault()
-        const {title, content, files} = this.state
+        // const {title, content, files} = this.state
         const formData = new FormData(event.target as HTMLFormElement)
         const response = await Api.insertPost(formData)
         console.log(response)
+
+        if(response.status) {
+            if(this.titleInputRef.current) {
+                this.titleInputRef.current.value = ""
+            }
+            if(this.contentInputRef.current) {
+                this.contentInputRef.current.value = ""
+            }
+            if(this.fileInputRef.current) {
+                this.fileInputRef.current.files = null
+            }
+        }
+
     }
 
     render() {
@@ -37,15 +59,15 @@ class AdminPanel extends Component<any, IState> {
                 <Form onSubmit={this.handleSubmit.bind(this)}>
                     <FormHeader>upload post</FormHeader>
 
-                    <FormField name={"title"} text={"title"}
+                    <FormTextInput name={"title"} text={"title"} ref={this.titleInputRef}
                                onChange={(e: ChangeEvent<HTMLInputElement>) => this.setState({title: e.target.value})}
                     />
 
-                    <FormField name={"content"} text={"post content"} as={"textarea"}
+                    <FormTextArea name={"content"} text={"post content"} ref={this.contentInputRef}
                                onChange={(e: ChangeEvent<HTMLTextAreaElement>) => this.setState({content: e.target.value})}
                     />
 
-                    <FormField name={"files"} text={"upload photos"} as={"file"}
+                    <FormFileInput name={"files"} text={"upload photos"} ref={this.fileInputRef}
                                onChange={(e: ChangeEvent<HTMLInputElement>) => this.setState({files: e.target.files})}
                     />
 
