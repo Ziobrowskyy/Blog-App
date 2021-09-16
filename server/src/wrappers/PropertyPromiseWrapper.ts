88368311288b04c7a8f6d91ae81e05e8c1d02d98
Ziobrowskyy <25063177+Ziobrowskyy@users.noreply.github.com>
2021-09-16 19:22:56 +0200

    Server::PropertyPromiseWrapper: Add fast resolution when property value is available.

--- server/src/wrappers/PropertyPromiseWrapper.ts
+++ server/src/wrappers/PropertyPromiseWrapper.ts
@@ -1,6 +1,8 @@
 export default function PropertyPromiseWrapper<T>(target: (...args: Array<any>) => undefined | T, interval: number = 100): Promise<T> {
     return new Promise(resolve => {
-        let value: any | undefined
+        let value: T | undefined = target()
+        if(value)
+            resolve(value)
         const timer = setInterval(() => {
             value = target()
             if (value) {
