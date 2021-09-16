fc256eea0f27dc773a61ee839b9b6f76ff702934
Ziobrowskyy <25063177+Ziobrowskyy@users.noreply.github.com>
2021-09-16 19:16:42 +0200

    Client::Login: Register button now redirects to /register, failed login attempt shows alert.

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,75 +1,79 @@
 import React, {ChangeEvent, Component, FormEvent} from "react"
-import {Api} from "../API"
+import {API} from "../API"
 import {FormTextInput} from "./FormField"
 import FormButton from "./FormButton"
 import FormHeader from "./FormHeader"
 import Form from "./Form"
 import "../styles/Login.scss"
+import Alert from "./Alert"
 
 interface IProps {
     onLogin: () => void
-}
-
-enum ActionType {
-    LOGIN,
-    REGISTER
+    onRegister: () => void
 }
 
 interface IState {
-    action: ActionType
     username: string
     password: string
+    showAlert: boolean
 }
 
 export default class Login extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
         this.state = {
-            action: ActionType.LOGIN,
             username: "",
-            password: ""
+            password: "",
+            showAlert: false
         }
     }
 
     async handleSubmit(e: FormEvent) {
+        console.log("SUBMIT")
         e.preventDefault()
-        const {username, password, action} = this.state
+        const {username, password} = this.state
+        try {
+            const response = await API.login({username, password})
+            console.log(response)
 
-        let response
-        if (action == ActionType.LOGIN) {
-            console.log("Login")
-            response = await Api.login({username, password})
-        } else {
-            console.log("Register")
-            response = await Api.register({username, password})
-        }
+            if (response.data.success)
+                this.props.onLogin()
+            else
+                this.setState({showAlert: true})
 
-        console.log(response)
-        if (response.data.success) {
-            this.props.onLogin()
+        } catch (e) {
+            this.setState({showAlert: true})
         }
+    }
+
+    onRegister(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault()
+        this.props.onRegister()
     }
 
     render() {
         return (
             <div className={"auth-wrapper"}>
+
+                {this.state.showAlert && <Alert type={"warn"}>Cannot log in!</Alert>}
+
                 <Form onSubmit={this.handleSubmit.bind(this)}>
                     <FormHeader>user login</FormHeader>
 
                     <FormTextInput text={"username"}
-                               onChange={(e: ChangeEvent<HTMLInputElement>) => this.setState({username: e.target.value})}
+                                   onChange={(e: ChangeEvent<HTMLInputElement>) => this.setState({username: e.target.value})}
                     />
 
                     <FormTextInput text={"password"} type={"password"}
-                               onChange={(e: ChangeEvent<HTMLInputElement>) => this.setState({password: e.target.value})}
+                                   onChange={(e: ChangeEvent<HTMLInputElement>) => this.setState({password: e.target.value})}
                     />
 
                     <div className={"button-wrapper"}>
-                        <FormButton variant={"primary"} onClick={__ => this.setState({action: ActionType.LOGIN})}>
+                        <FormButton variant={"primary"}>
                             log in
                         </FormButton>
 
-                        <FormButton onClick={__ => this.setState({action: ActionType.REGISTER})}>
+                        <FormButton type={"button"} onClick={this.onRegister.bind(this)}>
                             register
                         </FormButton>
                     </div>
